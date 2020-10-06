0f3e04a2265143c61b322acd2da9a426808e54e7
Estefania Pichardo <171G0242@rcarbonifera.tecnm.mx>
2020-10-06 11:59:10 -0500

    Actividades unidad 2

--- Practica2/Program.cs
+++ Practica2/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Unidad2ProgramacionWeb
+namespace Practica2
 {
     public class Program
     {
--- Practica2/Startup.cs
+++ Practica2/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Unidad2ProgramacionWeb
+namespace Practica2
 {
     public class Startup
     {
@@ -16,6 +16,7 @@ namespace Unidad2ProgramacionWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +26,12 @@ namespace Unidad2ProgramacionWeb
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseFileServer();//wwwroot
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
